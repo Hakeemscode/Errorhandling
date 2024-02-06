9755d26bb79da8f70069bd4583a021b2b2747ec4
atandaabdulhakeem862@gmail.com <atandaminat@gmail.com>
2024-02-06 12:44:47 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        //String builder class
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Hello");
+        stringBuilder.append("c");
+
+        System.out.println(stringBuilder + " Length " + stringBuilder.length());
+
+
+
+
+
+
+
+
+
+
+
+
+//        int[] number = {4, 8, 16, 32, 64, 128, 256, 512};
+//        int[] denom = {2, 0, 4, 4, 0, 8};
+//
+//        for (int i = 0; i < number.length; i++) {
+//            try {
+//                System.out.println(number[i] + "/" + denom[i] + " is " + number[i] / denom[i]);
+//
+//            } catch (ArithmeticException e) {
+//                System.out.println("Cant divide by zero");
+//                }catch (ArrayIndexOutOfBoundsException e){
+//                  System.out.println("No matching element found");
+//            }
+
+            }
+        }
+
+
+
+
+
